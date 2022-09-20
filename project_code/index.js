32c7d92d286771af90b9b869cd9bf69d26bdb5a5
sheikdon <sheikdonabdiresac@gmail.com>
2022-09-20 09:12:44 -0500

    finish css version 1

---
+++ project_code/index.js
@@ -0,0 +1,12 @@
+//welcome screen
+window.onload = function() {
+}
+window.onresize = function() {
+    gameloop.onresize();
+}
+
+//labeling pieces
+
+let playerOne = "black";
+let playerTwo = "white";
+let startPlayer = "playerOne";
