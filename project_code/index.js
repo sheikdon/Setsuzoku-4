1c1e6609122e794585abfc345402b9396accff54
sheikdon <sheikdonabdiresac@gmail.com>
2022-09-21 15:52:53 -0500

    day 3 done

--- project_code/index.js
+++ project_code/index.js
@@ -1,23 +1,38 @@
-//welcome screen
-window.onload = function() {
-}
-window.onresize = function() {
-    gameLoop.onresize();
-}
-//sounds
-const gameOverSound = new Audio("sounds/game_over.wav");
-const clickSound = new Audio("sounds/click.wav");
+// //welcome screen
+// window.onload = function() {
+// }
+// window.onresize = function() {
+//     gameLoop.onresize();
+// }
+// //sounds
+// const gameOverSound = new Audio("sounds/game_over.wav");
+// const clickSound = new Audio("sounds/click.wav");
+// circles.forEach((circle) => circle.addEventListener("click", gridClick));
 
-circles.forEach((circle) => circle.addEventListener("click", gridClick));
-//labeling pieces: use let variable since const messes it up.
-const circles = document.querySelector(".circle")
-var playerOne = "black";
-let playerTwo = "white";
-Let startPlayer = "playerOne";
+// //labeling pieces: use let variable since const messes it up.
+// const circles = document.querySelector(".circle")
+// const board = document.querySelector(".board")
+// const gameOverArea = document.getElementById("game-over-area");
+// const gameOverText = document.getElementById("game-over-text");
+// const playAgain = document.getElementById("play-again");
+// let playerOne = "black";
+// let playerTwo = "white";
+// let startPlayer = "playerOne";
+// // console.log('click', playerOne)
 
-//labeling board commands
-let gameOver = false;
+
+// //labeling board commands
+// let gameOver = false;
+// let board;
+// let rows = 6;
+// let columns = 7;
+// let currentColumns = [];
+
+//vr2
+const colors = {
+    '0': "black",
+    '1': "white",
+    '2': "grey"
+};
 let board;
-let rows = 6;
-let columns = 7;
-let currentColumns = []
+
