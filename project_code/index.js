57c532f355e18ae20a92c616ca7438a7aa793dce
sheikdon <sheikdonabdiresac@gmail.com>
2022-09-21 12:35:38 -0500

    labeled the board with javascript

--- project_code/index.js
+++ project_code/index.js
@@ -4,11 +4,20 @@ window.onload = function() {
 window.onresize = function() {
     gameLoop.onresize();
 }
+//sounds
+const gameOverSound = new Audio("sounds/game_over.wav");
+const clickSound = new Audio("sounds/click.wav");
 
-//labeling pieces
-let playerOne = "black";
+circles.forEach((circle) => circle.addEventListener("click", gridClick));
+//labeling pieces: use let variable since const messes it up.
+const circles = document.querySelector(".circle")
+var playerOne = "black";
 let playerTwo = "white";
-let startPlayer = "playerOne";
+Let startPlayer = "playerOne";
 
 //labeling board commands
-
+let gameOver = false;
+let board;
+let rows = 6;
+let columns = 7;
+let currentColumns = []
